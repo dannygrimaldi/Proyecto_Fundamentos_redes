835f5699c5fcde04ade5c1a01cb4da6f61a5da1b
dannygrimaldi <71056766+dannygrimaldi@users.noreply.github.com>
2024-02-03 16:36:40 -0600

    Eliminar archivos y actualizar modo de archivo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Error404 from './containers/errors/error404';
 import Home from './containers/pages/home';
-import SelectItems from "./containers/pages/SelectItems"
-import Submit from "./containers/pages/Submit"
-/* import Register from './containers/pages/Registro';
-import Autenticacion from "./containers/pages/Autenticacion";
-import SetdataTDC from "./containers/pages/SetdataTDC"
-import SetdataTDD from "./containers/pages/SetdataTDD"
-
- */
-import Loaderfiles from "./containers/pages/LoaderPDF";
 
 function App() {
   return (
@@ -17,15 +8,6 @@ function App() {
       <Routes>
       <Route path="*" element={<Error404/>}/>
       <Route path="/" element={<Home/>}/>
-{/*       <Route path="/login" element={<Autenticacion/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/set/tdc" element={<SetdataTDC/>}/>
-      <Route path="/set/tdd" element={<SetdataTDD/>}/>
-      <Route path="/Loader_files" element={<Loaderfiles/>}/>
-    <Route path="/selectItems" element={<SelectItems/>}/>
-      <Route path="/sendRequest" element={<Submit/>}/>
- */}   
-    
       </Routes>
     </Router>
   );
